59e6d4a3bb048c9ce3edfb0f66c78e735c97b61f
manarabdulahad <manarabdulahad@oakland.edu>
2025-11-18 15:37:52 -0500

    Add files via upload

---
+++ DigitsMethod.java
@@ -0,0 +1,24 @@
+public class DigitsMethod {
+
+    
+    public static void digits(int c) {
+        if (c == 0) {
+            System.out.print(0);
+            return;
+        }
+
+       
+        digits(c - 1);
+
+        
+        System.out.print(c);
+
+        
+        digits(c - 1);
+    }
+
+    
+    public static void main(String[] args) {
+        digits(3); // expected: 010201030102010
+    }
+}
