cc3d3100e8c2eb5f98a4cc00186420fe10434669
manarabdulahad <manarabdulahad@oakland.edu>
2025-11-18 15:41:57 -0500

    Add files via upload

---
+++ BinaryPrint.java
@@ -0,0 +1,28 @@
+public class BinaryPrint {
+
+    
+    public static void binaryPrint(int n) {
+      
+        if (n == 0) {
+            System.out.print(0);
+            return;
+        }
+
+        
+        if (n > 1) {
+            binaryPrint(n / 2);
+        }
+
+        
+        System.out.print(n % 2);
+    }
+
+    
+    public static void main(String[] args) {
+        binaryPrint(0);  
+        System.out.println();
+        binaryPrint(4);  
+        System.out.println();
+        binaryPrint(27); 
+    }
+}
